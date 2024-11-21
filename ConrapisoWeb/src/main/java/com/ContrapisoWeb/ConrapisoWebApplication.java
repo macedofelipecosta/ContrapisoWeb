ec61c1056aa8e0a1a10c3a47f46bef04c0082695
Felipe Macedo Costa <128637344+macedofelipecosta@users.noreply.github.com>
2024-11-21 12:59:21 -0300

    Initial commit

---
+++ ConrapisoWeb/src/main/java/com/ContrapisoWeb/ConrapisoWebApplication.java
@@ -0,0 +1,13 @@
+package com.ContrapisoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConrapisoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConrapisoWebApplication.class, args);
+	}
+
+}
---
+++ ConrapisoWeb/src/test/java/com/ContrapisoWeb/ConrapisoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ContrapisoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConrapisoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
